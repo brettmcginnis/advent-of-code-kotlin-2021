33d7f494636732e45ce604b390ac61d99e551f9a
Brett McGinnis <brett.mcginnis@hulu.com>
2021-12-01 12:07:05 -0800

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
+fun countIncreaseFromPrevious(input: List<Int>): Int {
+    var count = 0
+
+    input.forEachIndexed { index, current ->
+        val next = input.getOrNull(index + 1)
+
+        next?.let { if (it > current) count++ }
+    }
+
+    return count
+}
+
+fun sumForward(input: List<Int>): List<Int> {
+    val updated = mutableListOf<Int>()
+
+    input.forEachIndexed { index, current ->
+        updated.add(current + input.getOrElse(index + 1) { 0 } + input.getOrElse(index + 2) { 0 })
+    }
+
+    return updated
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val nums = input.map { it.toInt() }
+    println(countIncreaseFromPrevious(nums)) // part 1
+    println(countIncreaseFromPrevious(sumForward(nums))) // part 1
 }
