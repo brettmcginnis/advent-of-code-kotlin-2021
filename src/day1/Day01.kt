b1f443550747189a3b847124ddca2d75a8466fca
Brett McGinnis <brett.mcginnis@hulu.com>
2021-12-02 14:47:15 -0800

    Refactored day1 to be windowed

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -1,31 +1,13 @@
 package day1
 
-import readInput
+import java.io.File
 
-private fun countIncreaseFromPrevious(input: List<Int>): Int {
-    var count = 0
+private fun countIncreaseFromPrevious(input: List<Int>) = input.windowed(2).count { (a, b) -> a < b }
 
-    input.forEachIndexed { index, current ->
-        val next = input.getOrNull(index + 1)
-
-        next?.let { if (it > current) count++ }
-    }
-
-    return count
-}
-
-private fun sumForward(input: List<Int>): List<Int> {
-    val updated = mutableListOf<Int>()
-
-    input.forEachIndexed { index, current ->
-        updated.add(current + input.getOrElse(index + 1) { 0 } + input.getOrElse(index + 2) { 0 })
-    }
-
-    return updated
-}
+private fun sumForward(input: List<Int>): List<Int> = input.windowed(3).map { it.sum() }
 
 fun main() {
-    val input = readInput("Day01")
+    val input =  File("src/day1", "Day01.txt").readLines()
     val nums = input.map { it.toInt() }
     println(countIncreaseFromPrevious(nums)) // part 1
     println(countIncreaseFromPrevious(sumForward(nums))) // part 2
